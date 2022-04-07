01a60a081445de0ed06508a5530fa789df2d4d01
umcondo <superpowerkdg@gmail.com>
2022-04-07 10:31:14 +0900

    code review

--- 2022-4-6/index2.js
+++ 2022-4-6/index2.js
@@ -15,11 +15,30 @@ wNav.forEach((elm) => {
   });
 });
 
-function changeBar(e) {
-  //   console.log(e.offsetTop);
-  //   console.log(e.currentTarget.offsetTop);
-  document.getElementById("colorBase").style.top =
-    e.offsetTop + e.offsetHeight + "px";
-  document.getElementById("colorBase").style.left = e.offsetLeft + "px";
-  document.getElementById("colorBase").style.width = e.offsetWidth + "px";
+function moveBase(e, divId) {
+  redTarget = e.currentTarget;
+
+  document.getElementById(divId).style.top =
+    redTarget.offsetTop + redTarget.offsetHeight + "px";
+  document.getElementById(divId).style.left = redTarget.offsetLeft + "px";
+  document.getElementById(divId).style.width = redTarget.offsetWidth + "px";
 }
+
+function moveBase2(e, divId) {
+  blueTarget = e.currentTarget;
+
+  document.getElementById(divId).style.top =
+    redTarget.offsetTop + redTarget.offsetHeight + "px";
+  document.getElementById(divId).style.left = redTarget.offsetLeft + "px";
+  document.getElementById(divId).style.width = redTarget.offsetWidth + "px";
+}
+
+Target.forEach( (elm) => {elm.addEventListener("click", () => {moveBase(event,'colorBase3')})})
+Target2.forEach( (elm) => {elm.addEventListener("click", () => {moveBase(event,'colorBase4')})})
+
+
+//윈도우 창이 변화되면 현재 타겟을 다시 클릭하는 이벤트를 발생시키는 함수
+window.onresize = () => {
+  redTarget.click();
+  blueTarget.click();
+};
