0f458edf201aeb8b941ac73e01518e5806679d71
umcondo <superpowerkdg@gmail.com>
2022-05-18 17:57:54 +0900

    html form을 이용한 http, img dom 조작

---
+++ 2022-5-18/text.js
@@ -0,0 +1,5 @@
+<img src="img/tree-7186835.jpg" style="height: 20vh" alt="" />
+<img src="img/avenue-815297.jpg" style="height: 20vh" alt="" />
+<img src="img/garbage-5400780.jpg" style="height: 20vh" alt="" />
+<img src="img/lake-7194103.jpg" style="height: 20vh" alt="" />
+<img src="img/cat-7182671.jpg" style="height: 20vh" alt="" />
