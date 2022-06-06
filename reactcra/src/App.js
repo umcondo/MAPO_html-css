36f732bb533854f65acbfddff8074825c2dd1c97
umcondo <superpowerkdg@gmail.com>
2022-06-06 15:42:35 +0900

    벨로퍼트 11~15 배열인 상태 요소 추가, 삭제, 수정 styled-components props 다루기 useRef의 default value 활용

--- reactcra/src/App.js
+++ reactcra/src/App.js
@@ -1,12 +1,91 @@
+import { useRef, useState } from "react";
+
+// components
 import UseState from "./components/UseState";
 import InputSample from "./components/InputSample";
+import UserList from "./components/User";
+import CreateUser from "./components/CreateUser";
 
 function App() {
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      username: "velopert",
+      email: "public.velopert@gmail.com",
+      active: true,
+    },
+    {
+      id: 2,
+      username: "tester",
+      email: "tester@example.com",
+      active: true,
+    },
+    {
+      id: 3,
+      username: "liz",
+      email: "liz@example.com",
+      active: false,
+    },
+  ]);
+
+  const nextId = useRef(4);
+
+  const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      username,
+      email,
+    };
+    // setUsers([...users, user]);
+    setUsers(users.concat(user));
+
+    setInput({
+      username: "",
+      email: "",
+    });
+    nextId.current += 1;
+  };
+
+  const [input, setInput] = useState({
+    username: "",
+    email: "",
+  });
+
+  const { username, email } = input;
+
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setInput({
+      ...input,
+      [name]: value,
+    });
+  };
+
+  const onRemove = (id) => {
+    setUsers(users.filter((user) => user.id !== id));
+  };
+
+  const onToggle = (id) => {
+    setUsers(
+      users.map((user) =>
+        user.id === id ? { ...user, active: !user.active } : user
+      )
+    );
+  };
   return (
     <div className="App">
       <UseState />
       <br />
       <InputSample />
+      <br />
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
+      <hr />
+      <CreateUser
+        onCreate={onCreate}
+        onChange={onChange}
+        username={username}
+        email={email}
+      />
     </div>
   );
 }
---
+++ reactcra/src/components/User.js
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+const StyledCompo = styled.div`
+  cursor: pointer;
+  color: ${(props) => (props.active ? "green" : "black")};
+`;
+const User = ({ username, email, id, onRemove, active, onToggle }) => {
+  return (
+    <>
+      <StyledCompo active={active}>
+        <span onClick={() => onToggle(id)}>{username}</span>
+      </StyledCompo>
+      <span>{email}</span>
+      <button onClick={() => onRemove(id)}>삭제</button>
+    </>
+  );
+};
+
+function UserList({ users, onRemove, onToggle }) {
+  const numbers = [1, 2, 3, 4, 5];
+  let numId = 1;
+
+  return (
+    <div>
+      {users.map((elm) => (
+        <User {...elm} key={elm.id} onRemove={onRemove} onToggle={onToggle} />
+      ))}
+
+      {numbers.map((num) => (
+        <div key={numId++}>{num}</div>
+      ))}
+    </div>
+  );
+}
+export default UserList;
